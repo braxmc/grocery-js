9026382bafafef5dd67953499b28b6ef556e62c2
Braxton McClellan <thebraxmc@gmail.com>
2020-09-24 18:53:55 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = { items: [
+    { id: 1, itemName: 'Chips', complete: true },
+    { id: 2, itemName: 'Monster', complete: false},
+    { id: 3, itemName: 'Soap', complete: false},
+  ]}
+
+  visibleItems = () => {
+    const { items } = this.state
+    return items
+  }
+
+  render() {
+    return(
+      <div>
+        <header>S & B United</header>
+        <ul>
+          
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
